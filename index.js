5db9a85e30b3658055bd810f82b3e46316f3e7f3
NielsLouwes <nielslouwes45@gmail.com>
2021-04-19 21:20:24 +0200

    removed solution from main branch

--- index.js
+++ index.js
@@ -3,14 +3,11 @@
 const numbers = [9,4,5,6,7,3,2,6,4,3,2];
 
  const arrayLength = () => {
-    for (i = 0; i <numbers.length; i++);
-    const solution = numbers.length;
-    console.log(solution);
-        // return (numbers.length);
+    //Write me!
 
  }
 
-arrayLength();
+
 
 
 
